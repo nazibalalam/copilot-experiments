ecc7e39f895e785f03b04880b747bfc80bddba80
Nazib Al Alam <nazibalalam@gmail.com>
2022-01-29 17:56:08 +0600

    Initial Commit

---
+++ add.py
@@ -0,0 +1,62 @@
+# print the best linux distro
+def print_best_distro:
+    best_distro = "Arch"
+    print("The best linux distro is: " + best_distro)
+
+print_best_distro()
+
+# print the best windows version
+def print_windows_version:
+    best_windows_version = "10"
+    print("The best windows version is: " + best_windows_version)
+
+print_windows_version()
+
+# print the best mac version
+def print_mac_version:
+    best_mac_version = "Mojave"
+    print("The best mac version is: " + best_mac_version)
+
+print_mac_version()
+
+# print the best coding career
+def print_coding_career:
+    best_coding_career = "Full Stack"
+    print("The best coding career is: " + best_coding_career)
+
+print_coding_career()
+
+# print the best coding language
+def print_coding_language:
+    best_coding_language = "Python"
+    print("The best coding language is: " + best_coding_language)
+
+print_coding_language()
+
+# print the best coding framework
+def print_coding_framework:
+    best_coding_framework = "Django"
+    print("The best coding framework is: " + best_coding_framework)
+
+print_coding_framework()
+
+# print whether flutter or react native is the best
+def print_best_coding_framework:
+    best_coding_framework = "Flutter"
+    print("The best coding framework is: " + best_coding_framework)
+
+print_best_coding_framework()
+
+# print the best programmer
+def print_best_programmer:
+    best_programmer = "Me"
+    print("The best programmer is: " + best_programmer)
+
+print_best_programmer()
+
+# print whether copilot is awesome
+def print_copilot:
+    copilot = "Yes"
+    print("Copilot is awesome: " + copilot)
+
+print_copilot()
